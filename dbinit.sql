707471c1e418f4c065355f8a0fd8ebaf4d4404a5
Artiesy <artezka@gmail.com>
2022-12-26 05:03:43 +0300

    added docker stuff + nginx balancer

---
+++ dbinit.sql
@@ -0,0 +1,3 @@
+create table users (id integer, name varchar(64), studNumber varchar(24), age integer, isAdmin boolean);
+		insert into users (name, studNumber, age, isAdmin) values ('Admin User Vyacheslavovich', '7303-2022', 23, true);
+create table tasks (id integer, description varchar(2048), assigned integer);
